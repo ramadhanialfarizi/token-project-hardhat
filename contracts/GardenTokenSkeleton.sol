842833cd86fd90d346ee5a50e348e530fb0aa163
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-11-09 10:55:49 +0700

    add garden token skeleton

--- contracts/GardenTokenSkeleton.sol
+++ contracts/GardenTokenSkeleton.sol
@@ -1,4 +1,42 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.30;
 
-contract GardenTokenSkeleton {}
+contract GardenTokenSkeleton {
+    string public constant name = "Garden Token";
+    string public constant symbol = "GDN";
+    uint8 public constant decimals = 18;
+
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowances;
+    uint256 public totalSupply;
+
+    address public owner;
+    address public gameContract;
+
+    bool public paused;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+    event GameContractSet(address indexed gameContract);
+    event Paused(address account);
+    event Unpaused(address account);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this");
+        _;
+    }
+
+    modifier onlyGameContract() {
+        require(msg.sender == gameContract, "Only game contract can call this");
+        _;
+    }
+
+    modifier whenNotPaused() {
+        require(!paused, "Contract is paused");
+        _;
+    }
+}
